92427612c502e93aa304f831a6732e45e0673034
yowgf <alexthomasmol@gmail.com>
2022-05-30 12:51:39 -0300

    Add Hill Climbing algorithm

--- solver/_internal/policies/hclimb.py
+++ solver/_internal/policies/hclimb.py
@@ -1,13 +1,13 @@
-from ..datastruct.expansion_heap import ExpansionHeap
+from ..datastruct.queue import Queue
 from ..explorer.expansion import Expansion
 from ..explorer.explorer import Explorer
 from ..explorer.move import Move
+from .heuristics import manhattan_distance
 from ..log import log
 
 logger = log.logger()
 
-# TODO
-def hclimb(initial_state, goal_state):
+def hclimb(initial_state, goal_state, heuristic=manhattan_distance):
     logger.info("Starting Hill Climbling run")
     
     found_goal = False
@@ -16,12 +16,14 @@ def hclimb(initial_state, goal_state):
 
     explorer = Explorer(initial_state)
 
-    frontier = ExpansionHeap()
-    frontier.push(0, Expansion(initial_state, Move.moves_from(initial_state)))
+    # In the case of Hill Climbing, the frontier always just has one Expansion.
+    frontier = Queue()
+    frontier.push(Expansion(initial_state, Move.moves_from(initial_state)))
 
     while not found_goal and len(frontier) > 0:
-        _, expansion = frontier.pop()
-        
+        expansion = frontier.pop()
+
+        new_expansions = []
         for move in expansion.moves:
             expanded, state_new = explorer.expand(expansion.parent_state, move)
             if not state_new:
@@ -29,9 +31,18 @@ def hclimb(initial_state, goal_state):
             if expanded.parent_state == goal_state:
                 found_goal = True
                 break
+            new_expansions.append(expanded)
 
-            depth = explorer.state_depth(expanded.parent_state)
-            frontier.push(depth, expanded)
+        if len(new_expansions) <= 0:
+            break
+
+        heuristics = [heuristic(expansion.parent_state)
+                      for expansion in new_expansions]
+
+        expansion_with_best_heuristic = new_expansions[
+            heuristics.index(min(heuristics))]
+
+        frontier.push(expansion_with_best_heuristic)
 
     if found_goal:
         logger.info("Finished Hill Climbling run. Found goal.")
